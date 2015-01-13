8b402b77af0b05fd24a0b38a35e8a37c7dfccb49
Matt Walters <matt.walters@electronifie.com>
2015-01-13 14:10:01 -0500

    overriding default bunyan loggers to attempt to grab a transient correlationId from process.domain, if present. setting logger name to process title, unless title is "node". else setting to file name.

--- index.js
+++ index.js
@@ -12,6 +12,31 @@ try {
 function setLogger (name) {
   var logger = bunyan.createLogger({ name: name });
   logger.level(0);
+
+  var levels = ['trace', 'debug', 'info', 'warn', 'error', 'fatal' ];
+
+  levels.forEach(function (level) {
+
+    var fn = logger[level].bind(logger);
+
+    function _getCorrelationId () {
+      var args = Array.prototype.slice(arguments);
+      var obj = typeof args[0] === 'object' ? args[0] : {};
+      if (process.domain && process.domain.correlationId) {
+        obj.correlationId = process.domain.correlationId;
+      }
+      if (typeof args[0] === 'object') {
+        args[0] = obj;
+      } else {
+        args.unshift(obj);
+      }
+      fn.apply(logger, args);
+    }
+
+    logger[level] = _getCorrelationId;
+
+  });
+
   module.exports = logger;
   module.exports.setServiceName = function (name) {
     setLogger(name);
@@ -19,7 +44,7 @@ function setLogger (name) {
 }
 
 if (bunyan) {
-  setLogger(path.basename(process.argv[1]));
+  setLogger(path.basename(process.title !== 'node' ? process.title : process.argv[1]));
 } else {
   var levels = ['debug', 'info', 'warn', 'error'];
 

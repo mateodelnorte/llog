40a5f952292cb5c50ea9bf67c256eb4eb5962a39
Matt Walters <github@iammattwalters.com>
2022-09-13 01:36:11 -0500

    fix: updating typesript types.

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,17 @@
-declare type BunyanLogFunction = (formatMessage?: string, ...interpolationArg?: unknown) => void;
-declare type DebugLogFunction = (formatMessage?: string, ...interpolationArg?: unknown) => void;
-declare type PinoLogFunction = (merginObject?: unknown, formatMessage?: string, ...interpolationArg?: unknown) => void;
+declare type BunyanLogFunction = (formatMessage?: string, ...interpolationArg?: unknown[]) => void;
+declare type DebugLogFunction = (formatMessage?: string, ...interpolationArg?: unknown[]) => void;
+declare type PinoLogFunction = (merginObject?: unknown, formatMessage?: string, ...interpolationArg?: unknown[]) => void;
 declare type LlogFunction = BunyanLogFunction | DebugLogFunction | PinoLogFunction;
 
+declare interface logger {
+  trace: LlogFunction,
+  debug: LlogFunction,
+  info: LlogFunction,
+  warn: LlogFunction,
+  error: LlogFunction,
+  fatal: LlogFunction,
+}
+
 declare module "llog" {
-  export default {
-    trace: LlogFunction,
-    debug: LlogFunction,
-    info: LlogFunction,
-    warn: LlogFunction,
-    error: LlogFunction,
-    fatal: LlogFunction,
-  }
+  export default logger;
 }

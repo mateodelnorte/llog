ed44ec14be12f48840a09299097b9975dfba2e78
Matt Walters <github@iammattwalters.com>
2022-09-13 03:07:15 -0500

    fix: updating typesript types.

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,14 @@
-declare type LlogFunction = (merginObject?: string | Error | unknown, formatMessage?: string, ...interpolationArg: unknown[]) => void;
-
-declare interface logger {
-  trace: LlogFunction,
-  debug: LlogFunction,
-  info: LlogFunction,
-  warn: LlogFunction,
-  error: LlogFunction,
-  fatal: LlogFunction,
-}
-
 declare module "llog" {
+  declare type LlogFunction = (merginObject?: string | Error | unknown, formatMessage?: string, ...interpolationArg: unknown[]) => void;
+
+  declare interface logger {
+    trace: LlogFunction,
+    debug: LlogFunction,
+    info: LlogFunction,
+    warn: LlogFunction,
+    error: LlogFunction,
+    fatal: LlogFunction,
+  }
+
   export default logger;
 }
